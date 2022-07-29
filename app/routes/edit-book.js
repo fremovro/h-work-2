e145700f183d445d8e7a85736ec0bcba8c86ba9b
Roman <fremov.ro@mail.ru108451187+fremovro@users.noreply.github.com>
2022-07-29 17:09:02 +0500

    commit

--- app/routes/edit-book.js
+++ app/routes/edit-book.js
@@ -5,7 +5,9 @@ export default Route.extend({
     dataService: service('data'),
 
     model({ id }) {
-        console.log(window.location.href );
+        // console.log(window.location.href );
+        // console.log(this.router.currentURL); // returns null
+        // console.log(this.router.get('currentURL')); // returns null
         return this.get("dataService").get_book(id);
     }
 });
