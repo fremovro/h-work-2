209abbf72d587eb99f5ce8cc33196b40ab6723d5
Roman <fremov.ro@mail.ru108451187+fremovro@users.noreply.github.com>
2022-08-14 15:50:52 +0500

    h-work-4-commit-1

--- app/components/book-item.js
+++ app/components/book-item.js
@@ -1,17 +1,21 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
+
 export default Component.extend({
     dataService: service('data'),
+    store: service('store'),
 
     init() {
         this._super(...arguments);
+
         // this.set('tags', A()); 
     },
 
     actions: {
         async deleteBook(book) {
             await book.destroyRecord();
+            this.get('store').unloadRecord(book);
         },
     }
 });
--- app/controllers/add-speaker.js
+++ app/controllers/add-speaker.js
@@ -11,17 +11,9 @@ export default Controller.extend({
                 surname: this.get('speakerSurname'),
                 patronymic: this.get('speakerPatronymic'),
             };
-            // speakerModel.set('name', this.get('speakerName'));
-            // speakerModel.set('surname', this.get('speakerSurname'));
-            // speakerModel.set('patronymic', this.get('speakerPatronymic'));
             let newSpeaker = this.get('store').createRecord('speaker', speakerModel);
             newSpeaker.serialize();
             await newSpeaker.save();
-            // await this.get("dataService").createSpeaker({
-            //     name: this.get('speakerName'),
-            //     surname: this.get('speakerSurname'),
-            //     patronymic: this.get('speakerPatronymic'),
-            // });
             this.transitionToRoute('speaker');
         }
     }
